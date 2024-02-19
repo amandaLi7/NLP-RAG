ff3b6d2cf275f8c09ed2527e2313d3383f564c0f
emilyguo23 <epguo@andrew.cmu.edu>
2024-02-19 11:54:41 -0500

    Added other required files, reformatted data folder

--- process_pdf.py
+++ process_pdf.py
@@ -3,17 +3,39 @@ import pypdf as pdf
 import pdfplumber as plumb
 from pypdf import PdfWriter
 
-# Merge PDFs
-merger = PdfWriter()
-
-for file in os.listdir('./data/pdf'):
-    file_path = os.path.join('./data/pdf', file)
-    doc = open(file_path, "rb")
-
-    # Document parsing here
+# Main document cleaning function
+def parse_pdf(file):
     # TODO
+    pass
 
-    merger.append(doc)
 
-merger.write("./data/pdf/merged-pdf.pdf")
-merger.close()
+def process_pdfs(directory_path, merger):
+
+    for file in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, file)
+        doc = open(file_path, "rb")
+
+        # Document parsing
+        # TODO
+
+        merger.append(doc)
+
+    return merger
+
+
+def merger_write(merger, file_path):
+    merger.write(file_path)
+    merger.close()
+
+
+if __name__ == "__main__":
+
+    merger = PdfWriter()
+
+    input_dir = "./data/pdfs"
+
+    process_pdfs(input_dir, merger)
+
+    output_file = "./data/pdfs/parsed.pdf"
+
+    merger_write(merger, output_file)
